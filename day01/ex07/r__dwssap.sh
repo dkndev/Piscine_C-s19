5150b8c437e409e41835b7cd8a4027a49f313d6d
Nelis De kerpel <nde-kerp@e-r5-p5.s19.be>
2018-08-07 11:24:02 +0200

    reversing login,sort alfb, comma sparated, end with dote

--- day01/ex07/r__dwssap.sh
+++ day01/ex07/r__dwssap.sh
@@ -1,1 +1,2 @@
-cat /etc/passwd | grep -v '^#' | cut -f 1 -d ':' | head -n +"$FT_LINE2" | tail -n+"$FT_LINE1"
+cat /etc/passwd | grep -v '^#' | cut -f 1 -d ':' | sed -n "n;p" | rev | sort -r | head -n +"$FT_LINE2" | tail -n+"$FT_LINE1" | paste -s -d"," - | sed 's/,/, /g' | sed 's/$/./'
+
