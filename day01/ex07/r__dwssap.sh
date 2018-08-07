0f01ad4f52de93dc1f6e2d77a1ee83d83478d77a
Nelis De kerpel <nde-kerp@e-r5-p5.s19.be>
2018-08-07 11:56:36 +0200

    add comments

--- day01/ex07/r__dwssap.sh
+++ day01/ex07/r__dwssap.sh
@@ -1,2 +1,15 @@
+##
+# remove comments -> grep -v '^#'
+# extract login name -> cut -f 1 -d ':'
+# ever other line starting form the second (odd) -> sed -n "n;p"
+# reverse login name (nelis -> silen) -> rev
+# sort alfb and reverse -> sort -r
+# between line numbers -> head -n +"$FT_LINE2" | tail -n+"$FT_LINE1"
+# comma separated -> paste -s -d"," -
+# add space after comma -> sed 's/,/, /g'
+# end with a '.' (dote) sed 's/$/./'
+##
+
+
 cat /etc/passwd | grep -v '^#' | cut -f 1 -d ':' | sed -n "n;p" | rev | sort -r | head -n +"$FT_LINE2" | tail -n+"$FT_LINE1" | paste -s -d"," - | sed 's/,/, /g' | sed 's/$/./'
 
